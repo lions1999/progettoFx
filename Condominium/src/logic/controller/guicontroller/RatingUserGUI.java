13c08bfdcacf552b29e41772d136b9f97abd3a9d
lions1999 <davideleoni99@gmail.com>
2021-11-24 10:03:47 +0100

    modifiche davide

--- Condominium/src/logic/controller/guicontroller/RatingUserGUI.java
+++ Condominium/src/logic/controller/guicontroller/RatingUserGUI.java
@@ -3,9 +3,7 @@ package logic.controller.guicontroller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Slider;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -24,8 +22,12 @@ public class RatingUserGUI implements Initializable {
         sliderRating.setStyle("-fx-font-size: 20");
     }
 
-    public void submitRate(MouseEvent mouseEvent) {
+    public void submitRate() {
         RateValue.setText(String.valueOf((int)sliderRating.getValue()));
         RateValue.setVisible(true);
+    }
+
+    public void submitRating() {
+
     }
 }
