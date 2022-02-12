58e49754087e5a0064bc7f6628a2fc20b117613d
luke9598 <moretti.luca1998@gmail.com>
2022-02-12 17:18:35 +0100

    todo btn update rate

---
+++ Condominium/src/logic/controller/guicontroller/second/admin/condominium/UpdateRatingItemGUI.java
@@ -0,0 +1,8 @@
+package logic.controller.guicontroller.second.admin.condominium;
+
+import javafx.event.ActionEvent;
+
+public class UpdateRatingItemGUI {
+    public void onDeletePress(ActionEvent actionEvent) {
+    }
+}
