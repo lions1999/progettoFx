6143a7ec7a573f9ce609b2cadef2c85beee284e9
luke9598 <moretti.luca1998@gmail.com>
2022-02-13 23:13:10 +0100

    smell

--- Condominium/src/logic/engineeringclasses/dao/FeeDAO.java
+++ Condominium/src/logic/engineeringclasses/dao/FeeDAO.java
@@ -46,17 +46,17 @@ public class FeeDAO extends SqlDAO{
 
     public void addFees(FeeBean fee,String table) throws SQLException{
         try{
-            String sql = "INSERT INTO "+table+" (fee_apt,fee_admin,fee_park,fee_elevator,fee_pet,fee_wifi) VALUES (?,?,?,?,?,?,?,?,?)";
+            String sql = "INSERT INTO "+table+" (fee_apt,fee_admin,fee_park,fee_elevator,fee_pet,fee_wifi) VALUES (?,?,?,?,?,?)";
             preset = prepConnect(sql);
             preset.setString(1,fee.getApt());
             preset.setDouble(2,fee.getAdmin());
-            if(fee.getPark().equals(0.0)) preset.setNull(6, Types.NULL);
+            if(fee.getPark().equals(0.0)) preset.setNull(3, Types.NULL);
             else preset.setDouble(3,fee.getPark());
-            if(fee.getElevator().equals(0.0)) preset.setNull(7, Types.NULL);
+            if(fee.getElevator().equals(0.0)) preset.setNull(4, Types.NULL);
             else preset.setDouble(4,fee.getElevator());
-            if(fee.getPet().equals(0.0)) preset.setNull(8, Types.NULL);
+            if(fee.getPet().equals(0.0)) preset.setNull(5, Types.NULL);
             else preset.setDouble(5,fee.getPet());
-            if(fee.getWifi().equals(0.0)) preset.setNull(9, Types.NULL);
+            if(fee.getWifi().equals(0.0)) preset.setNull(6, Types.NULL);
             else preset.setDouble(6,fee.getWifi());
             preset.execute();
         } finally {
@@ -70,13 +70,13 @@ public class FeeDAO extends SqlDAO{
             String sql = "UPDATE "+table+" SET fee_admin=?, fee_park=?, fee_elevator=?, fee_pet=?, fee_wifi=? WHERE fee_apt='"+fee.getApt()+"'";
             preset = prepConnect(sql);
             preset.setDouble(1,fee.getAdmin());
-            if(fee.getPark().equals(0.0)) preset.setNull(5, Types.NULL);
+            if(fee.getPark().equals(0.0)) preset.setNull(2, Types.NULL);
             else preset.setDouble(2,fee.getPark());
-            if(fee.getElevator().equals(0.0)) preset.setNull(6, Types.NULL);
+            if(fee.getElevator().equals(0.0)) preset.setNull(3, Types.NULL);
             else preset.setDouble(3,fee.getElevator());
-            if(fee.getPet().equals(0.0)) preset.setNull(7, Types.NULL);
+            if(fee.getPet().equals(0.0)) preset.setNull(4, Types.NULL);
             else preset.setDouble(4,fee.getPet());
-            if(fee.getWifi().equals(0.0)) preset.setNull(8, Types.NULL);
+            if(fee.getWifi().equals(0.0)) preset.setNull(5, Types.NULL);
             else preset.setDouble(5,fee.getWifi());
             preset.execute();
         } finally {
